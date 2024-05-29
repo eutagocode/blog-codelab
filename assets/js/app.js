7495ef981a9bf30e759f2c577f10d675aea5584c
eutagocode <thiagoluix21@gmail.com>
2024-05-29 13:51:04 -0300

    feat: :sparkles: Criação da função de filtrar por um card do blog e foi feita a configuração do scrollreveal para o header e os cards

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,6 @@
 const allHearts = document.querySelectorAll(".icon-heart");
+const allTitles = document.querySelectorAll(".title");
+const inputSearchElement = document.getElementById("input-search");
 
 for (const heart of allHearts) {
     heart.addEventListener("click", () => {
@@ -9,3 +11,33 @@ for (const heart of allHearts) {
         }
     });
 }
+
+inputSearchElement.addEventListener("input", () => {
+    const inputSearch = inputSearchElement.value.toLowerCase();
+
+    const titlesArray = Array.from(allTitles);
+
+    const filteredTitles = titlesArray.filter((title) =>
+        title.textContent.toLowerCase().includes(inputSearch)
+    );
+
+    for (const title of titlesArray) {
+        const card = title.parentElement;
+        card.style.display = "none";
+    }
+
+    for (const title of filteredTitles) {
+        const card = title.parentElement;
+        card.style.display = "grid";
+    }
+});
+
+let sr = ScrollReveal({
+    duration: 2500,
+    distance: "60px",
+});
+
+sr.reveal(".header-container", { origin: "top", delay: 500 });
+sr.reveal(".card-1", { origin: "left", delay: 600 });
+sr.reveal(".card-2", { origin: "right", delay: 700 });
+sr.reveal(".card-3", { origin: "bottom", delay: 800 });
